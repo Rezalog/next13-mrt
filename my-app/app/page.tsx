db672af42cfe29ef4e1411de03ff6ddee2825a6a
Rezal K <rladlfdnd369@naver.com>
2023-06-21 06:28:27 +0000

    first

---
+++ my-app/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <div className='container'>
+      <Link href='/'>Home</Link>
+      <Link href='root'
+
+    </div>
+  )
+}
