e6185779e5101fb4bc359d2dd86449a0e4614c8e
Rezal K <rladlfdnd369@naver.com>
2023-06-21 06:43:29 +0000

    second

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,12 +1,50 @@
+import { MRT_ColumnDef, MaterialReactTable } from 'material-react-table'
 import Image from 'next/image'
 import Link from 'next/link'
+import { useMemo } from 'react'
+
+interface Person {
+  name: string;
+  age: number;
+}
+
+const data: Person[] = [
+  {
+    name: 'John',
+    age: 30,
+  },
+  {
+    name: 'Sara',
+    age: 25,
+  },
+];
+
 
 export default function Home() {
-  return (
-    <div className='container'>
-      <Link href='/'>Home</Link>
-      <Link href='root'
 
-    </div>
+  const columns = useMemo<MRT_ColumnDef<Person>[]>( 
+    () => [
+      {
+        accessorKey: 'name',
+        header: 'Name',
+        muiTableHeadCellProps: { sx: { color: 'green' } },
+      },
+      {
+        accessorFn: (originalRow) => originalRow.age,
+        id: 'age',
+        header: 'Age',
+        Header: <i style={{ color: 'red' }}>Age</i>,
+      },
+    ],
+    [],
+  );
+
+  return (
+    <MaterialReactTable
+      columns={columns}
+      data={data}
+      enableRowSelection
+      enableColumnOrdering
+      enableGlobalFilter={false}/>
   )
 }
