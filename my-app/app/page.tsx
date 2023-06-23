6ab5b37f89cb87ac45485dc914c11bdfca768692
Rezal K <rladlfdnd369@naver.com>
2023-06-23 03:00:49 +0000

    FEAT(composedchart)

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -4,30 +4,7 @@ import { MRT_ColumnDef, MaterialReactTable } from 'material-react-table'
 import Image from 'next/image'
 import Link from 'next/link'
 import { Fragment, useMemo } from 'react'
-import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
-
-export function chart() {
-
-    const data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400}];
-
-    const renderLineChart = (
-      <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-        <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-        <XAxis dataKey="name" />
-        <YAxis />
-      </LineChart>
-    );
-
-    return (
-
-        <div>
-            { renderLineChart }
-        </div>
-    )
-    
-}
-
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, BarChart, Tooltip, Legend, Bar, ComposedChart, Area } from 'recharts';
 
 interface Person {
   name: string;
@@ -110,21 +87,60 @@ export default function Home() {
     [],
   );
 
+    const chartData = [
+      {name: ' A', uv: 400, pv: 2400, amt: 1400},
+      {name: ' B', uv: 600, pv: 1500, amt: 6400},
+      {name: ' C', uv: 800, pv: 2000, amt: 5400},
+      {name: ' D', uv: 100, pv: 1000, amt: 8400},
+      {name: ' E', uv: 400, pv: 1400, amt: 900},
+      {name: ' F', uv: 500, pv: 8400, amt: 1000},
+      {name: ' G', uv: 200, pv: 2400, amt: 800},
+      {name: ' H', uv: 900, pv: 12400, amt: 3000},
+      {name: ' I', uv: 300, pv: 400, amt: 300},
+      {name: ' J', uv: 100, pv: 100, amt: 400},
+      
+    ]
+
   return (
     <div>
-      <div className='chart'>
-        chart
-      </div>
 
-      <div className='grid'>
-        <MaterialReactTable
-          columns={columns}
-          data={data}
-          enableRowSelection
-          enableColumnOrdering
-          enableGlobalFilter={false}/>
-      </div>
+      <ComposedChart width={730} height={250} data={data}>
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <CartesianGrid stroke="#f5f5f5" />
+        <Area type="monotone" dataKey="amt" fill="#8884d8" stroke="#8884d8" />
+        <Bar dataKey="pv" barSize={20} fill="#413ea0" />
+        <Line type="monotone" dataKey="uv" stroke="#ff7300" />
+      </ComposedChart>
 
+      
+      {/* <LineChart width={600} height={300} data={chartData} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+        <Line type="natural" dataKey="uv" stroke="#8884d8" />
+        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+        <XAxis dataKey="name" />
+        <YAxis aria-modal='false'/>
+
+        
+      </LineChart>
+    
+      <BarChart width={730} height={250} data={chartData}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="pv" fill="#8884d8" />
+          <Bar dataKey="uv" fill="#82ca9d" />
+        </BarChart> */}
+
+      <MaterialReactTable
+        columns={columns}
+        data={data}
+        enableRowSelection
+        enableColumnOrdering
+        enableGlobalFilter={false}/>
     </div>
   )
 }
